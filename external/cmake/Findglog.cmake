47aad2336501a47d06e037bc6013d5a36c982392
Phil <phytoporg@gmail.com>
2022-07-23 14:57:23 -0700

    Clean up Findglog.cmake

--- external/cmake/Findglog.cmake
+++ external/cmake/Findglog.cmake
@@ -14,23 +14,14 @@ include(FindPackageHandleStandardArgs)
 set(GLOG_ROOT_DIR "" CACHE PATH "Folder contains Google glog")
 
 if(WIN32)
-	#    find_path(GLOG_INCLUDE_DIR glog/logging.h
-	#        PATHS ${GLOG_ROOT_DIR}/src/windows)
+    # Nothing to do here in our current Windows build solution
 else()
     find_path(GLOG_INCLUDE_DIR glog/logging.h
         PATHS ${GLOG_ROOT_DIR})
 endif()
 
 if(MSVC)
-	#    find_library(GLOG_LIBRARY_RELEASE libglog_static
-	#        PATHS ${GLOG_ROOT_DIR}
-	#        PATH_SUFFIXES Release)
-	#
-	#    find_library(GLOG_LIBRARY_DEBUG libglog_static
-	#        PATHS ${GLOG_ROOT_DIR}
-	#        PATH_SUFFIXES Debug)
-	#
-	#    set(GLOG_LIBRARY optimized ${GLOG_LIBRARY_RELEASE} debug ${GLOG_LIBRARY_DEBUG})
+    # Nothing to do here in our current Windows build solution
 else()
     find_library(GLOG_LIBRARY glog
         PATHS ${GLOG_ROOT_DIR}
