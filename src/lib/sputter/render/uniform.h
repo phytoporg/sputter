0485fcd761d8835cd38241dbda9ddaf64d09d5a5
Phil <phytoporg@gmail.com>
2022-07-26 21:16:40 -0700

    uniform.cpp, uniform.h initial revision

---
+++ src/lib/sputter/render/uniform.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace sputter { namespace render {
+    template<typename T>
+    class Uniform
+    {
+    public:
+        static void Set(uint32_t slot, const T& value);
+        static void Set(uint32_t slot, T* array, uint32_t arrayLength);
+        static void Set(uint32_t slot, std::vector<T>& v);
+
+    private:
+        Uniform() = delete;
+        Uniform(const Uniform& uniform) = delete;
+        Uniform& operator=(const Uniform& uniform) = delete;
+        ~Uniform() = delete;
+    };
+
+    template<typename T>
+    void Uniform<T>::Set(uint32_t slot, const T& value)
+    {
+        Set(slot, static_cast<T*>(&value));
+    }
+
+    template<typename T>
+    void Uniform<T>::Set(uint32_t slot, std::vector<T>& v)
+    {
+        Set(slot, v.data(), static_cast<uint32_t>(v.size()));
+    }
+} }
