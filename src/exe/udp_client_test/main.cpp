e17eec80b42787afa5b42fbd98cb06aafbf7342f
Jack Michaud <jack@lomz.me>
2023-02-04 10:29:16 -0500

    feat: punchthrough client

--- src/exe/udp_client_test/main.cpp
+++ src/exe/udp_client_test/main.cpp
@@ -4,27 +4,89 @@
 
 using namespace sputter::net;
 
-// Client code
-int main() {
-  sputter::net::UDPPort client;
+// Constants for RX port number, TX port number, and remote host
+const int RX_PORT = 50000;
+const int TX_PORT = 50001;
+const char* REMOTE_HOST = "127.0.0.1";
 
-  // Open the client port for transmitting data
-  if (!client.open(50000)) {
+// Example of receiving data:
+// std::string address;
+// uint16_t port;
+
+// int bytesReceived = receivingPort.receive(buffer, 256, address, port);
+
+// if (bytesReceived > 0) {
+//   std::cout << "Received " << bytesReceived << " bytes from " << address << ":" << port << std::endl;
+//   std::cout << "Data: " << buffer << std::endl;
+// }
+
+
+// Client code
+int main(int argc, char* argv[]) {
+  // Parse arguments for remote host
+  if (argc > 1) {
+    REMOTE_HOST = argv[1];
+  } else {
+    std::cout << "Usage: " << argv[0] << " [remote host]" << std::endl;
+    return 1;
+  }
+
+  // Open the client port for receiving data (hole punch)
+  sputter::net::UDPPort receivingPort;
+
+  if (!receivingPort.open(RX_PORT)) {
     std::cerr << "Error: Failed to open client port\n";
     return 1;
   }
 
   // Send data to the server
-  const char *message = "Hello, server!";
-  // TODO Find out why this isn't sending data. Are we not flushing?
-  if (!client.send(message, strlen(message) + 1, "127.0.0.1", 50001)) {
+  const char *message = "hole-punch";
+  if (!receivingPort.send(message, strlen(message) + 1, REMOTE_HOST, TX_PORT)) {
     std::cerr << "Error: Failed to send data\n";
     return 1;
-  } else {
-    std::cout << "Sent message: " << message << std::endl;
   }
-  // flush
-  client.close();
+
+  // Open the client port for sending data
+  sputter::net::UDPPort sendingPort;
+
+  if (!sendingPort.open(TX_PORT)) {
+    std::cerr << "Error: Failed to open client port\n";
+    return 1;
+  }
+
+  // Send data until the server sends a message back
+  // First, send a message to the server using the sending port
+  // Then, receive a message from the server using the receiving port
+  // Repeat until a message from the server is received.
+  char buffer[256];
+  std::string address;
+  uint16_t port;
+
+  while (true) {
+    // Send a message to the server
+    const char *message = "Hello, server!";
+    if (!sendingPort.send(message, strlen(message) + 1, REMOTE_HOST, RX_PORT)) {
+      std::cerr << "Error: Failed to send data\n";
+      return 1;
+    }
+
+    // Receive a message from the server
+    int bytesReceived = receivingPort.receive(buffer, 256, address, port);
+
+    if (bytesReceived > 0) {
+      std::cout << "Received " << bytesReceived << " bytes from " << address << ":" << port << std::endl;
+      std::cout << "Data: " << buffer << std::endl;
+      break;
+    }
+
+    // Send another messaage to the server
+    message = "Hello again, server!";
+    if (!sendingPort.send(message, strlen(message) + 1, REMOTE_HOST, RX_PORT)) {
+      std::cerr << "Error: Failed to send data\n";
+      return 1;
+    }
+  }
+
 
   return 0;
 }
